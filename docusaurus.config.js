fbc5dfc20771426b58080dfe374269687514a03b
yinsel <yinsel@163.com>
2024-04-14 19:23:11 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,170 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Yinselの小窝',  // -----------------------网站标题-----------------------
+  tagline: '记录知识的地方，避免遗忘。',   // -----------------------网站描述-----------------------
+  favicon: 'img/yinsel.png',
+  customFields: {
+    "button": "Go",
+    "docs": "knowledge",
+    "mainTitle": "主页"
+  },
+  url: 'https://home.yinsel.top/',   // -----------------------你的主页-----------------------
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'yinsel', // -----------------------GitHub用户名-----------------------
+  projectName: 'yinsel.github.io', // -----------------------GitHub仓库-----------------------
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: "知识库",
+          routeBasePath: "/knowledge",
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/yinsel/yinsel.github.io/tree/main/',   //-----------------------GitHub编辑URL-----------------------
+            showLastUpdateTime: true,
+            showLastUpdateAuthor: true,
+        },
+        blog: {
+          path: "博客",
+          routeBasePath: "/blog",
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/yinsel/yinsel.github.io/tree/main/',  //-----------------------GitHub编辑URL-----------------------
+            blogTitle: "博客",
+            blogSidebarTitle: "文章列表",
+          blogSidebarCount: "ALL"
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+  themes: [
+    [
+      // @ts-ignore
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      // @ts-ignore
+      ({
+        hashed: true,   // 是否对搜索结果进行hash
+        language: ["en", "zh"], // 搜索语言
+        indexDocs: true, // 是否对docs进行索引
+        indexBlog: true, // 是否对blog进行索引
+        indexPages: true, // 是否对pages进行索引
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Yinselの小窝',   // -----------------------网站标题-----------------------
+        logo: {
+          alt: 'Yinsel Logo',
+          src: 'img/yinsel.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '知识库',
+          },
+          {to: '/blog', label: '博客', position: 'left'},
+          {
+            href: 'https://github.com/yinsel/yinsel.github.io',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 6,
+      },
+      footer: {
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()} Yinsel`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+		additionalLanguages: ['java','csharp','php'],
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/comment.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Giscus from "@giscus/react";
+import {useColorMode} from "@docusaurus/theme-common"; // 导入当前主题 API
+
+export default function Comment() {
+  const {colorMode} = useColorMode(); // 获取当前主题
+  return (
+    // 前面放一个带 margin 的 div，美观
+	//-----------------------Giscus评论-----------------------
+    <div style={{marginTop: "30px"}}>
+      <Giscus
+        repo=""
+        repoId=""
+        category=""
+        categoryId="" 
+        mapping="title"
+        term="Welcome to @giscus/react component!"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme={colorMode} // 根据当前主题设置
+        lang="zh-CN"
+        loading="lazy"
+        crossorigin="anonymous"
+        async
+      />
+    </div>
+  );
+}
